c725f48675979a0e858f8992b215191fe0290b4c
Cy12334 <165625761+Cy12334@users.noreply.github.com>
2024-04-03 20:31:06 +0800

    Add files via upload

---
+++ Number_1_to_100/Number_1_to_100/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Number_1_to_100;
+
+public static class Program 
+{
+    public static void Main (string [] args)
+    {
+        int i = 1;
+        while (i <=100)
+        {
+            Console.WriteLine(i);
+            i++;
+
+            if (i % 3 == 0)
+            {
+                Console.WriteLine("Hello");
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("Goodbye");
+                    if (i % 3 % 5 == 0)
+                    { 
+                    Console.WriteLine("Hello Goodbye");
+                    }
+                }
+            }
+        }
+     }
+}
